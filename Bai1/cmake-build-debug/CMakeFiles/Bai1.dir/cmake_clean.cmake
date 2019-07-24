71e57e9d8359b62a9cef469aa235597df1e7bf04
Huy1411 <53205463+Huy1411@users.noreply.github.com>
2019-07-24 17:13:27 +0700

    Create main.c.obj

---
+++ Bai1/cmake-build-debug/CMakeFiles/Bai1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Bai1.dir/main.c.obj"
+  "Bai1.pdb"
+  "Bai1.exe"
+  "Bai1.exe.manifest"
+  "libBai1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Bai1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
